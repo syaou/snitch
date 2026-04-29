cfbb86d6ebea8e7d153d0b4e0a1f8e4ca34ba511
syaou <sanayousefi06@gmail.com>
2026-04-29 11:16:11 +1000

    Initial Commit

---
+++ Snitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snitch
+//
+//  Created by Sana Yousefi on 29/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snitch/SnitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnitchApp.swift
+//  Snitch
+//
+//  Created by Sana Yousefi on 29/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct SnitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
